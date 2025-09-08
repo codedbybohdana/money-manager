d1d37c4e5884879853572d856ba48ff74d9a2943
codedbybohdana <ttttffzxcvbnm111@gmail.com>
2025-09-08 10:44:39 +0200

    updated2

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,7 @@
-import { colors } from "@/constants/theme";
-import { useRouter } from "expo-router";
-import React, { useEffect } from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 const Index = () => {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        resizeMode="contain"
-        source={require("../assets/images/hand-with-money.webp")}
-      />
-    </View>
-  );
+  return <View></View>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: colors.textLight,
-    gap: 20,
-  },
-  logo: {
-    height: "20%",
-    aspectRatio: 1,
-  },
-});
 export default Index;
--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,30 +1,9 @@
 import { colors, spacingX, spacingY } from "@/constants/theme";
-import { useAuth } from "@/contexts/authContext";
-import useFetchData from "@/hooks/useFetchData";
-import { WalletType } from "@/types";
 import { horizontal, vertical } from "@/utils/styling";
-import { orderBy, where } from "firebase/firestore";
 import React from "react";
 import { ImageBackground, StyleSheet, View } from "react-native";
 import Typo from "./Typo";
 const HomeCard = () => {
-  const { user } = useAuth();
-  const { data: wallets, error } = useFetchData<WalletType>("wallets", [
-    where("uid", "==", user?.uid),
-    orderBy("created", "desc"),
-  ]);
-
-  const getTotals = () => {
-    return wallets.reduce(
-      (totals: any, item: WalletType) => {
-        totals.balance = totals.balance + Number(item.amount);
-        totals.income = totals.income + Number(item.totalIncome);
-        totals.expenses = totals.expenses + Number(item.totalExpenses);
-        return totals;
-      },
-      { balance: 0, income: 0, expenses: 0 }
-    );
-  };
   return (
     <ImageBackground
       source={require("../assets/images/card.png")}
